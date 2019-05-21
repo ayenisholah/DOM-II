f7cee659970328d4f726e352c9faa21e365f400a
Shola Ayeni <ayenisholah@yahoo.com>
2019-05-21 12:47:25 +0100

    mouseover modified

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ navLinks.forEach(element => {
     element.addEventListener("mouseover", function(event){
         event.target.style.color = "#A0001E"
     });
-});
-
-
+    setTimeout(function() {
+        event.target.style.color = "";
+    }, 5);
+}, false);
