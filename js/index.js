a9ad98360c2f5edc9a4196caa72fc2f4f273fc06
Shola Ayeni <ayenisholah@yahoo.com>
2019-05-21 13:15:24 +0100

    wheel event added

--- js/index.js
+++ js/index.js
@@ -12,3 +12,17 @@ function logKey(e) {
     log.textContent += ` ${e.code}`;
 }
 
+function zoom(event) {
+    event.preventDefault();
+  
+    scale += event.deltaY * -0.01;
+  
+    scale = Math.min(Math.max(.125, scale), 4);
+  
+    funBus.style.transform = `scale(${scale})`;
+}
+  
+let scale = 1;
+const funBus = document.querySelector('header img');
+console.log(funBus);
+funBus.onwheel = zoom;
