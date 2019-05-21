62fac3ef2757dcf8ff6102db23392c6c97b0026d
Shola Ayeni <ayenisholah@yahoo.com>
2019-05-21 15:52:45 +0100

    Scroll

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const navLinks = document.querySelectorAll('a');
+let navLinks = document.querySelectorAll('a');
 
 navLinks.forEach(element => {
     element.addEventListener("mouseover", function(event){
@@ -24,5 +24,32 @@ function zoom(event) {
   
 let scale = 1;
 const funBus = document.querySelector('header img');
-console.log(funBus);
 funBus.onwheel = zoom;
+
+const adventureImg = document.querySelector('.img-content img');
+
+adventureImg.className = "transition";
+
+adventureImg.addEventListener('transitionstart', function(element){
+    element.transform = "rotate"
+});
+
+let last_known_scroll_position = 0;
+let ticking = false;
+
+function doSomething(scroll_pos) {
+  
+}
+
+window.addEventListener('scroll', function(e) {
+  last_known_scroll_position = window.scrollY;
+
+  if (!ticking) {
+    window.requestAnimationFrame(function() {
+      doSomething(last_known_scroll_position);
+      ticking = false;
+    });
+
+    ticking = true;
+  }
+});
