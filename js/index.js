1a92b43da7f93379c076b8af7d005626264a1770
Shola Ayeni <ayenisholah@yahoo.com>
2019-05-22 15:20:29 +0100

    dblclick event added

--- js/index.js
+++ js/index.js
@@ -26,30 +26,7 @@ let scale = 1;
 const funBus = document.querySelector('header img');
 funBus.onwheel = zoom;
 
-const adventureImg = document.querySelector('.img-content img');
-
-adventureImg.className = "transition";
-
-adventureImg.addEventListener('transitionstart', function(element){
-    element.transform = "rotate"
-});
-
-let last_known_scroll_position = 0;
-let ticking = false;
-
-function doSomething(scroll_pos) {
-  
-}
-
-window.addEventListener('scroll', function(e) {
-  last_known_scroll_position = window.scrollY;
-
-  if (!ticking) {
-    window.requestAnimationFrame(function() {
-      doSomething(last_known_scroll_position);
-      ticking = false;
-    });
-
-    ticking = true;
-  }
-});
+const logoHeading = document.querySelector('.logo-heading');
+logoHeading.addEventListener('dblclick', function(event){
+  event.target.style.transform = "rotate(180deg)"
+})
